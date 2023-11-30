2a0071ee120f3cb74216f6970da6f37ae1dcc8b6
gabscrobson <gmta1410@gmail.com>
2023-11-30 17:57:28 -0300

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,14 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      yellow: {
+        DEFAULT: '#fdfd96',
       },
+      white: '#FFFFFF',
+      black: '#000000',
     },
   },
   plugins: [],
